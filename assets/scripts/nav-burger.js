14b22ea1006838a20cbb5479c4219b8f315e4fb6
LGodelyte <laura.godelyte@gmail.com>
2019-01-07 12:44:37 +0200

    Add

--- assets/scripts/nav-burger.js
+++ assets/scripts/nav-burger.js
@@ -4,18 +4,22 @@
 
 $(document).ready(function(){
 	$('.burger').click(function(){
-		$('.menu-item').toggleClass('show'),
+		$('.main-nav').toggleClass('show');
+	});
 
-	});
-	$('.burger').click(function(){
-		$('.grey-nav').toggleClass('show');
-	});
 	$('.menu-item').click(function(){
-		$('.main-nav').toggleClass('hidden');
+		$('.main-nav').toggleClass('show');
 	});
 });
 
+// window.onscroll = function() {scrollFunction()};
 
-
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     $('.grey-nav').toggleClass('show');
+//   } else {
+//    	$('.grey-nav').toggleClass('hidden');
+//   }
+// }
 
 }());
